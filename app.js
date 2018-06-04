7993ab9ee14c81decfe855306801f5132b4e213a
kurachy <lucassuhner@yahoo.fr>
2018-06-04 14:11:13 +0200

    setup basic authentification

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
+passport = require("passport"),
+User = require("./models/user.js"),
+LocalStrategy = require("passport-local"),
+passportLocalMongoose = require("passport-local-mongoose"),
 mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost/irfa");
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+  secret: "",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 var irfaSchema = new mongoose.Schema({
   image: String,
@@ -31,7 +47,6 @@ app.get("/", function(req, res){
 
 });
 
-
 app.post("/", function(req, res){
   var image = req.body.image;
   var desc = req.body.desc;
@@ -47,9 +62,58 @@ app.post("/", function(req, res){
   })
 });
 
+app.get("/secret",isLoggedIn, function(req, res){
+   res.render("secret");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secret");
+    });
+  });
+});
+
+
+
 app.get("/new", function(req, res){
   res.render("new.ejs");
 });
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+
+
+
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,function(req, res){
+
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login")
+}
 
 app.listen(3000, function(){
   console.log("Server started");
